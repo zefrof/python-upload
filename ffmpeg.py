926557a98434dcbc939dd0b075c1bb271c46c8c2
zefrof <zefrof+github@protonmail.com>
2021-06-02 21:48:14 -0500

    Added thumbnail generation

--- ffmpeg.py
+++ ffmpeg.py
@@ -1,19 +1,21 @@
 import glob
 import subprocess
-import datetime
+from datetime import datetime
+from PIL import Image, ImageFont, ImageDraw 
 
-#delete old output file
+#delete old output files
 subprocess.run("rm output.mkv", shell=True)
+subprocess.run("rm thumbnail.jpg", shell=True)
 
 #Download clips from Reddit
 subprocess.run('youtube-dl $(curl -s -H "User-agent: \'your bot 0.1\'" https://www.reddit.com/r/IdiotsInCars/hot.json?limit=20 | jq \'.\' | grep url_overridden_by_dest | grep -Eoh "https:\/\/v\.redd\.it\/\w{13}")', shell=True)
 
 #Check clips for kids or YT gets mad
-kidCheck = input("Are the clips free of kids? (y/N): ")
-if kidCheck == "y":
-	pass
-else:
-	quit()
+#kidCheck = input("Are the clips free of kids? (y/N): ")
+#if kidCheck == "y":
+#	pass
+#else:
+#	quit()
 
 video_files = glob.glob("*.mp4")
 
@@ -54,12 +56,39 @@ command += 'concat=n=' + str(len(video_files)) + ':v=1:a=1 [v] [a]" -map "[v]" -
 #print(command)
 subprocess.run(command, shell=True)
 
+#Get episode number
+now = datetime.now()
+og_date = datetime.strptime('2021-02-28', "%Y-%m-%d")
+str_now = now.strftime("%Y-%m-%d")
+epNum = str((now - og_date).days)
+
+#Create screen shot
+video_input_path = 'output.mkv'
+img_output_path = 'thumbnail.jpg'
+subprocess.call(['ffmpeg', '-i', video_input_path, '-ss', '00:00:05.000', '-vframes', '1', img_output_path])
+
+x, y = 15, 15
+fillcolor = "white"
+shadowcolor = "black"
+text = "Idiots In Cars " + epNum
+
+thumbnail = Image.open("thumbnail.jpg")
+font = ImageFont.truetype('Anton/Anton-Regular.ttf', 200)
+image_edit = ImageDraw.Draw(thumbnail)
+
+#https://stackoverflow.com/questions/41556771/is-there-a-way-to-outline-text-with-a-dark-line-in-pil
+image_edit.text((x-5, y-5), text, font = font, fill = shadowcolor)
+image_edit.text((x+5, y-5), text, font = font, fill = shadowcolor)
+image_edit.text((x-5, y+5), text, font = font, fill = shadowcolor)
+image_edit.text((x+5, y+5), text, font = font, fill = shadowcolor)
+
+image_edit.text((x, y), text, font = font, fill = fillcolor)
+thumbnail.save("thumbnail.jpg")
+
 #Upload video
 #https://github.com/tokland/youtube-upload
-now = datetime.datetime.now()
-now = now.strftime("%Y-%m-%d")
-epNum = '47'
-subprocess.run('youtube-upload --title="Best Of Idiots In Cars ' + epNum + '" --description="The best videos of Idiots In Cars from ' + now + '. Like, comment, and subscribe! New video every day!\nWe take the best clips of idiots in cars, and combine them into the ultimate idiots in cars compilation! Witness epic car crashes, police cars in action, and some of the top car driving fails from Reddit." --category="Autos & Vehicles" --tags="idiots in cars, best of idiots in cars, idiots in cars highlights, idiots in cars top, idiots in cars reddit, idiots in cars compilation, total idiots driving in russia, idiots in cars police, idiots in cars clips, idiots in cars driving fails, epic car fails, idiots in car, idiot in car, epic car crashes, dash cam footage, totoal idiots on the road, car crash compilation, epic dash cam video, best of idiots in cars ' + epNum + '" --default-language="en" --default-audio-language="en" --client-secrets="client_secrets.json" --credentials-file="my_credentials.json" output.mkv', shell=True)
+
+subprocess.run('youtube-upload --title="Best Of Idiots In Cars ' + epNum + '" --description="The best videos of Idiots In Cars from ' + str_now + '. Like, comment, and subscribe! New video every day!\nWe take the best clips of idiots in cars, and combine them into the ultimate idiots in cars compilation! Witness epic car crashes, police cars in action, and some of the top car driving fails from Reddit." --category="Autos & Vehicles" --tags="idiots in cars, best of idiots in cars, idiots in cars highlights, dashcam fails, idiots in cars reddit, idiots in cars compilation, total idiots driving in russia, idiots in cars police, idiots in cars clips, idiots in cars driving fails, epic car fails, idiots in car, car crash compilation, epic car crashes, dash cam footage, totoal idiots on the road, car crash compilation, epic dash cam video, road rage, best of idiots in cars ' + epNum + '" --default-language="en" --default-audio-language="en" --client-secrets="client_secrets.json" --credentials-file="my_credentials.json" output.mkv', shell=True)
 
 #Delete un-needed files
 subprocess.run("rm fixed/*.mp4", shell=True)
