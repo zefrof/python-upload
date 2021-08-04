6551e1a0dc40db46f1da5a3875f3432c6a9c8a25
zefrof <zefrof+github@protonmail.com>
2021-08-04 10:47:49 -0400

    More functions, alt timestamps for thumbnails

--- ffmpeg.py
+++ ffmpeg.py
@@ -1,5 +1,6 @@
 import glob
 import subprocess
+import requests
 from datetime import datetime
 from PIL import Image, ImageFont, ImageDraw 
 
@@ -22,25 +23,12 @@ def downloadClips():
 
 	subprocess.run(command, shell=True)
 
-def main():
-	#delete old output files
-	subprocess.run("rm output.mkv", shell=True)
-	subprocess.run("rm thumbnail.jpg", shell=True)
-
-	#Download clips from Reddit
-	downloadClips()
-
-	#Check clips for kids or YT gets mad
-	#kidCheck = input("Are the clips free of kids? (y/N): ")
-	#if kidCheck == "y":
-	#	pass
-	#else:
-	#	quit()
-
+#add silent audio track to videos with no audio
+#https://stackoverflow.com/questions/12368151/adding-silent-audio-in-ffmpeg#12375018
+def addSilentAudio():
 	video_files = glob.glob("*.mp4")
+	#video_files = video_files + glob.glob("*.mkv") <-- need to change subprocess.run to use arguments for this to work properly
 
-	#add silent audio track to videos with no audio
-	#https://stackoverflow.com/questions/12368151/adding-silent-audio-in-ffmpeg#12375018
 	audio_command = "ffmpeg -f lavfi -i anullsrc=channel_layout=stereo:sample_rate=44100 -i "
 	audio_command_back = " -c:v copy -c:a aac -shortest "
 
@@ -49,19 +37,20 @@ def main():
 		subprocess.run(audio_command + f + audio_command_back + "audio/" + str(index) + ".mp4", shell=True)
 		index += 1
 
+#convert all videos to the same sar and resolution
+#https://unix.stackexchange.com/questions/190431/convert-a-video-to-a-fixed-screen-size-by-cropping-and-resizing
+def fixRes():
 	video_files = glob.glob("audio/*.mp4")
 
-	#convert all videos to the same sar and resolution
-	#https://unix.stackexchange.com/questions/190431/convert-a-video-to-a-fixed-screen-size-by-cropping-and-resizing
 	index = 0
 	for f in video_files:
 		subprocess.run('ffmpeg  -i ' + f + ' -vf "scale=1920:1080:force_original_aspect_ratio=decrease,pad=1920:1080:-1:-1,setsar=1" -c:v mpeg4 -q:v 4 -c:a libmp3lame -q:a 4 fixed/' + str(index)  + ".mp4", shell=True)
 		index += 1
 
-	video_files = glob.glob("fixed/*.mp4")
-
-	#Combine all clips into one video
+def combineClips():
 	#https://stackoverflow.com/questions/7333232/how-to-concatenate-two-mp4-files-using-ffmpeg#11175851
+	video_files = glob.glob("fixed/*.mp4")
+	
 	command = "ffmpeg"
 
 	for f in video_files:
@@ -73,19 +62,18 @@ def main():
 		command += '[' + str(f) + ':v] [' + str(f) + ':a] '
 
 	command += 'concat=n=' + str(len(video_files)) + ':v=1:a=1 [v] [a]" -map "[v]" -map "[a]" output.mkv'
-	#print(command)
 	subprocess.run(command, shell=True)
 
-	#Get episode number
-	now = datetime.now()
-	og_date = datetime.strptime('2021-03-04', "%Y-%m-%d")
-	str_now = now.strftime("%Y-%m-%d")
-	epNum = str((now - og_date).days)
-
-	#Create screen shot
+def thumbnailGen(epNum):
 	video_input_path = 'output.mkv'
 	img_output_path = 'thumbnail.jpg'
-	subprocess.call(['ffmpeg', '-i', video_input_path, '-ss', '00:00:05.000', '-vframes', '1', img_output_path])
+	
+	if (int(epNum) % 2) == 0:
+		timestamp = '00:00:05.000'
+	else:
+		timestamp = '00:00:07.000'
+
+	subprocess.call(['ffmpeg', '-i', video_input_path, '-ss', timestamp, '-vframes', '1', img_output_path])
 
 	x, y = 50, 15
 	fillcolor = "white"
@@ -105,6 +93,38 @@ def main():
 
 	image_edit.text((x, y), text, font = font, fill = fillcolor)
 	thumbnail.save("thumbnail.jpg")
+
+def main():
+	#delete old output files
+	subprocess.run("rm output.mkv", shell=True)
+	subprocess.run("rm thumbnail.jpg", shell=True)
+
+	#Download clips from Reddit
+	downloadClips()
+
+	#Check clips for kids or YT gets mad
+	#kidCheck = input("Are the clips free of kids? (y/N): ")
+	#if kidCheck == "y":
+	#	pass
+	#else:
+	#	quit()
+
+	#Add silent audio track to videos
+	addSilentAudio()
+	#Adjust resoluton and sar to match
+	fixRes()
+
+	#Combine all clips into one video
+	combineClips()
+
+	#Get episode number
+	now = datetime.now()
+	og_date = datetime.strptime('2021-03-20', "%Y-%m-%d")
+	str_now = now.strftime("%Y-%m-%d")
+	epNum = str((now - og_date).days)
+
+	#Create screen shot
+	thumbnailGen(epNum)
 
 	#Upload video
 	#https://github.com/tokland/youtube-upload
